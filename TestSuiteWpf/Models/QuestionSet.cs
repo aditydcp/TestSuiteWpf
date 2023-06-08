7051c95394fb56a22c8d2b7c42a13a420805b979
Aditya Cahyo <aditydcp@gmail.com>
2023-06-08 23:44:08 +0700

    update get question algorithm

--- TestSuiteWpf/Models/QuestionSet.cs
+++ TestSuiteWpf/Models/QuestionSet.cs
@@ -11,9 +11,10 @@ namespace TestSuiteWpf.Models
         public List<Question> questions { get; set; }
         public int Level { get; }
 
-        private int lastQuestionGivenIndex = -1;
         // saves the index of the latest (max 5) given questions
-        private List<int> questionIndexBuffer = new(5);
+        private readonly static int bufferCapacity = 5;
+        private List<int> questionIndexBuffer = new();
+
         private readonly Random random = new();
 
         public QuestionSet(List<Question> questions, int level)
@@ -23,24 +24,39 @@ namespace TestSuiteWpf.Models
         }
 
         /// <summary>
-        /// Pick a random question from this question set
+        /// Pick a random question from this question set.
+        /// Question picked won't be the same as the previous 5 questions given.
         /// </summary>
         /// <returns>A <see cref="Question"/> object</returns>
         public Question GetRandomQuestion()
         {
             bool isNew = false;
             int index = 0;
-            // ensure the next question is not the same one as the previous
+            // ensure the next question is not the same one as the previous 5 questions
             while (!isNew)
             {
                 index = random.Next(0, questions.Count);
-                if (index != lastQuestionGivenIndex)
-                {
-                    isNew = true;
-                }
+                isNew = IsNewIndex(questionIndexBuffer, index);
             }
-            lastQuestionGivenIndex = index;
+
+            questionIndexBuffer = UpdateIndexBuffer(questionIndexBuffer, index);
             return questions.ElementAt(index);
+        }
+
+        private static bool IsNewIndex(List<int> indexBuffer, int newIndex)
+        {
+            foreach (int index in indexBuffer)
+            {
+                if (index == newIndex) return false;
+            }
+            return true;
+        }
+
+        private static List<int> UpdateIndexBuffer(List<int> indexBuffer, int newIndex)
+        {
+            if (indexBuffer.Count > bufferCapacity) { indexBuffer.RemoveAt(0); }
+            indexBuffer.Add(newIndex);
+            return indexBuffer;
         }
     }
 }
